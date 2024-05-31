7a07a81b96f56668b103a9d398afec693c2ad9be
0nnu <142360951+0nnu@users.noreply.github.com>
2024-05-31 14:29:14 +0300

    Update help.js

--- basicCommands/help.js
+++ basicCommands/help.js
@@ -2,7 +2,7 @@ const { EmbedBuilder, ActionRowBuilder, ButtonBuilder } = require('discord.js');
 const { ButtonStyle } = require('discord.js');
 const db = require("../mongodb");
 module.exports = {
-  name: 'help',
+  name: 'اغاني',
   aliases: ['hlp', 'h'],
   description: 'Shows a list of available commands',
   execute(message, args) {
@@ -28,7 +28,7 @@ module.exports = {
         // Music commands category
         {
           name: '▶️  Music',
-          value: '`play`, `stop`, `history`,`volume`,`pause`,`resume`,`247`',
+          value: '`شغل`, `طف`, `السجل`,`الصوت`,`وقف`,`كمل`,`247`',
           inline: true,
         },
         //fun category
